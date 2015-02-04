be6d3eee18e67973ce9a0eae0f12e4ac6e6cb1fe
unknown <kphongph@gmail.com>
2015-02-04 17:40:15 +0700

    init

---
+++ server/bin/discover_schema.js
@@ -0,0 +1,13 @@
+var path = require('path');
+var app = require(path.resolve(__dirname, '../server'));
+
+var dataSource = app.dataSources.hostDs;
+
+dataSource.discoverSchema('Host', {schema: 'dbo'},
+    function(err, schema) {
+  if (err) throw err;
+
+  console.log(JSON.stringify(schema, null, '  '));
+
+  dataSource.disconnect();
+});
